b4ff90a8333ad36488095dc68b71f884cfa685ea
HeapOfPackrats <eugenesurfs@gmail.com>
2018-02-16 20:44:04 -0800

    Completed day 5

---
+++ day5.py
@@ -0,0 +1,28 @@
+#http://adventofcode.com/2017/day/5
+
+import sys
+
+def main(argv):
+    #get input, otherwise prompt for input
+    if len(argv) == 2:
+        inputFile = open(argv[1])
+    else:
+        print("Please specify a file (day5.py [filepath])")
+        return
+
+    #see url at top for specifications
+    jumpList = list(map(int, inputFile.read().split()))
+    jumpCount = 0
+    index = 0
+    offset = 0
+
+    while index <= (len(jumpList) - 1):
+        offset = jumpList[index]
+        jumpList[index] += 1
+        index += offset
+        jumpCount += 1
+
+    print("It took {} steps to reach the exit".format(jumpCount))
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ day5b.py
@@ -0,0 +1,31 @@
+#http://adventofcode.com/2017/day/5
+
+import sys
+
+def main(argv):
+    #get input, otherwise prompt for input
+    if len(argv) == 2:
+        inputFile = open(argv[1])
+    else:
+        print("Please specify a file (day5b.py [filepath])")
+        return
+
+    #see url at top for specifications
+    jumpList = list(map(int, inputFile.read().split()))
+    jumpCount = 0
+    index = 0
+    offset = 0
+
+    while index <= (len(jumpList) - 1):
+        offset = jumpList[index]
+        if offset >= 3:
+            jumpList[index] -= 1
+        else:
+            jumpList[index] += 1
+        index += offset
+        jumpCount += 1
+
+    print("It took {} steps to reach the exit".format(jumpCount))
+
+if __name__ == "__main__":
+    main(sys.argv)
